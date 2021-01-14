3eab449d2f3f9bbc4273ffae934aefd60380d060
Alba599 <57162043+Alba599@users.noreply.github.com>
2021-01-14 11:30:06 -0600

    forecast

---
+++ src/DayCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import './DayCard.css';
+// import { mockComponent } from 'react-dom/test-utils';
+
+// function DayCard() {
+//     return<h1>Hi</h1>
+// }
+
+var moment = require('moment');
+
+const DayCard = ({reading}) => {
+    let newDate = new Date();
+    const weekday = reading.dt * 1000
+    newDate.setTime(weekday)
+
+    const imgURL = `owf owf-${reading.weather[0].id} owf-5x`
+
+    return(
+        
+        // <div className='card'>
+        //     <h3>{moment(newDate).format('dddd')}</h3>
+        //     <p>{moment(newDate).format('MMMM Do, h:mm a')}</p>
+        //     <h2>{Math.round(reading.main.temp)} °F</h2>
+        //     <p>{reading.weather[0].description}</p>
+        // </div>
+    )
+}
+
+
+
+export default DayCard;
--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -1,18 +1,60 @@
 import React from 'react';
+import DayCard from './DayCard';
+// import './DayCard.css';
+// const WeekContainer = () => {
+    // let [setResObj] = useState({});
+
+//     function getWeekContainer(e) {
+//         e.preventDefault();
+//         const weatherURL =
+//             'http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=655cbab7ac24dd1c334f705fd1da079a'
+//         fetch(weatherURL)
+//             .then(res => res.json())
+//             // .then(res => setResObj(res))
+//             .then(data => console.log("Data List Loaded", data.list))
+//           }
+//     return(
+//         <div>
+//             <h2>Hi</h2>
+//             {/* <p>{getWeekContainer}</p> */}
+//         </div>
+//     )
+// }
+
+
+
 
 class WeekContainer extends React.Component {
-    render() {
+
+    state = {
+        fullData: [],
+        dailyData: []
+    }
+    
         componentDidMount = () => {
             const weatherURL =
-            `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${apiConfig.owmKey}`
+            'http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=655cbab7ac24dd1c334f705fd1da079a'
         fetch(weatherURL)
             .then(res => res.json())
-            .then(data => console.log("Data List Loaded", data.list))
-          }
-          
+            .then(data => {
+                const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+                this.setState({
+                    fullData: data.list,
+                    dailyData: dailyData
+                }, () => console.log(this.state))
+                // console.log("Data List Loaded", res.list)
+            })
+    }
+
+    formatDayCards = () => {
+        return this.state.dailyData.map((reading, index) => <DayCard reading={reading} key={index} />)
+      }
+
+    render() {
         return (
             <div>
-                <h1>Hello</h1>
+                <h1>5-Day Weather Forecast</h1>
+                <div>{this.formatDayCards()}</div>
             </div>
         )
     }
